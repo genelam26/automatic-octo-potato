5a64caea94e95506b5e92b12b0accfead20fd8d9
Gene Lam <gene.lam@macaulay.cuny.edu>
2020-06-24 15:46:58 -0400

    first commit

---
+++ hello.py
@@ -0,0 +1,11 @@
+# This is how you comment a line of text
+# import os
+# os.system('osascript send.scpt {} "{}"'.format(7188134058, "hi"))
+""""
+This is how you comment a block of text
+"""
+
+print("helloworld\n")
+print("helloworld")
+print("helloworld\n")
+
